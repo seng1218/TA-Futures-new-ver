3c4a2c6fee5adb9131257c13dccd626dcb168edd
seng1218 <seng121887@gmail.com>
2026-05-14 12:48:02 +0800

    style: change page transition to zoom effect

--- src/components/PageTransition.tsx
+++ src/components/PageTransition.tsx
@@ -10,10 +10,10 @@ export default function PageTransition({ children }: { children: React.ReactNode
     <AnimatePresence mode="wait">
       <motion.div
         key={pathname}
-        initial={{ opacity: 0, y: 15 }}
-        animate={{ opacity: 1, y: 0 }}
-        exit={{ opacity: 0, y: -15 }}
-        transition={{ duration: 0.45, ease: [0.22, 1, 0.36, 1] }}
+        initial={{ opacity: 0, scale: 0.96 }}
+        animate={{ opacity: 1, scale: 1 }}
+        exit={{ opacity: 0, scale: 1.04 }}
+        transition={{ duration: 0.5, ease: [0.22, 1, 0.36, 1] }}
         className="flex flex-col min-h-screen"
       >
         {children}
